d443117ae4e07fa9bf3b80c6d488dc189bfa957c
alyrojas <alynerojas@gmail.com>
2024-07-11 22:46:19 -0600

    ejercicios

---
+++ bin/async_await_streams.dart
@@ -0,0 +1,7 @@
+//Archivo async_await_streams.dart
+void main() async{
+  Stream<int> stream = Stream.periodic(Duration(seconds: 1), (contador)=> contador).take(5); //Stream.periodic
+  await for  (var data in stream){
+    print("Data recibida: $data");
+  } 
+}
